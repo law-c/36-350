0d0339103f066e2c5fff08a72ebab9bbba313c8b
Lawrence Chen <law.chen117@gmail.com>
2017-12-09 23:24:09 -0500

    hello aliens file

---
+++ hello_aliens.R
@@ -0,0 +1,14 @@
+cat("Dear Aliens,\n
+    I am sending this message on behalf of the human race. 
+    First and foremost, we are humans; we require oxygen
+    and water to survive. We breathe oxygen, however, we
+    can only drink water. Our total population is about 
+    7 billion human individuals.We would like to know why you
+    are here? Why did you travel this far? How large is
+    your race? What galaxy / solar system are you from?
+    Are you willing to share some of your technology? 
+    Have you mastered Faster Than Light travel? What basic
+    needs do you require to survive?
+    
+    Thank you for hearing me out and we look forward to an 
+    amicable relationship.")
